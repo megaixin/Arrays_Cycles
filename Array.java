3a0e824e8315f3f38e3c1aca7c580833119426ea
megaixin <megaixin12@mail.ru>
2017-05-24 15:14:55 +0300

    right version

--- Array.java
+++ Array.java
@@ -1,41 +1,28 @@
 //найти наибольшее число в массиве, вернуть -999, если массив нулевой длины"
 class Array{
 	public static void main(String[]args){
-		int []myArr = {5,8,1,1};
-		//int [] mySecondArr = new int[0];
+		//int []myArr = {5,8,1,1};
+		int [] mySecondArr = new int[0];
 		
-		System.out.println(returnMaxValue(myArr));
+		System.out.println(returnMaxValue(mySecondArr));
 	}
 
 	public static int returnMaxValue(int[] inArr){
-		for(int i =0;i<inArr.length;i++){
+		//если массив нулевой
+		if (inArr.length == 0){
+			//возвращаем -999
+			return -999;
 			
-			//если массив нулевой
-			if (inArr.length == 0){
-				//возвращаем -999
-				return -999;
-			
-			}else{
+		}else{
 			//вычисляем наибольшее значение
-				int maxValueOfArr = inArr[0];
+		int maxValueOfArr = inArr[0];
+			for(int i = 0;i<inArr.length;i++){
+			
 				if(maxValueOfArr<inArr[i]){
 					maxValueOfArr = inArr[i];
 				}	
-				return maxValueOfArr;
 			}	
-		}
+			return maxValueOfArr;
+		}	
 	}
-
-	/*если массив нулевой
-		возвращаем -999
-	если массив не нулевой
-		вычисляем его наибольшее значение
-    возвращаем его*/
-	
-	/*или
-	если массив нулевой
-    возвращаем -999
-иначе
-    вычисляем наибольшее
-    возвращаем*/
 }
