a9c23af71794f33e2166c2217a96cb38c415df91
megaixin <megaixin12@mail.ru>
2017-05-24 16:39:10 +0300

    other vers of method-return

--- Array.java
+++ Array.java
@@ -1,13 +1,20 @@
 //найти наибольшее число в массиве, вернуть -999, если массив нулевой длины"
 class Array{
 	public static void main(String[]args){
-		//int []myArr = {5,8,1,1};
+		//int []myArr = {5,8,1,2};
 		int [] mySecondArr = new int[0];
 		
 		System.out.println(returnMaxValue(mySecondArr));
 	}
 
 	public static int returnMaxValue(int[] inArr){
-		return -1;
+		//кладем в переменную -999
+		int outResult = -999;
+		//если массив не пустой
+		if(inArr.length != 0){
+			//ищем наибольшее
+			//кладем в переменную наибольшее	
+		}
+		//возвращаем эту переменную
 	}
 }
