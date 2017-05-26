1f91ebf08f41e0133a7bd03b62640d9a5380f4fb
megaixin <megaixin12@mail.ru>
2017-05-26 14:55:47 +0300

    vers 0.4

--- IncreaseElementArr.java
+++ IncreaseElementArr.java
@@ -4,13 +4,12 @@ class IncreaseElementArr{
 
 		int [] myArr = {6,44,5,4,14,0,11};
 		writeArrayOnConsole(myArr);
-		increaseElementOfArray(myArr,2);
+		increaseElementOfArray(myArr,4);
 		writeArrayOnConsole(myArr);
 	}
-	public static void increaseElementOfArray( int[]inArr, int inElenent){
-		
-		inArr[inElenent] = inArr[inElenent]*10;
-
+	public static void increaseElementOfArray( int[]inArr, int inElement){
+        double var = inArr[inElement]*1.1;
+		inArr[inElement] = (int)( Math.round(var));
 	}
 
 	public static void writeArrayOnConsole(int[] innerMassive){
@@ -19,8 +18,11 @@ class IncreaseElementArr{
 			System.out.print(innerMassive[i]+" ");
 		}
 		System.out.println();
-	
 	}
 	
-	
+	/*
+	14/100 = 0.14 - один процент
+	0.14*10 = 1.4 - 10 процентов
+	14 + 1.4 = 15.4
+	*/
 }
