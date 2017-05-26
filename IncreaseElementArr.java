4a79e5d47499e4b71a3b48670d60504f04473847
megaixin <megaixin12@mail.ru>
2017-05-26 11:14:32 +0300

    ver 0.1

--- IncreaseElementArr.java
+++ IncreaseElementArr.java
@@ -1,17 +1,17 @@
 //Напишите метод, который будет увеличивать заданный элемент массива на 10%.
 class IncreaseElementArr{
-	public static void maim (String[]args){
+	public static void main (String[]args){
 
 		int [] myArr = new int {6,44,5,4,14,0,11};
-	
+		increaseElrmrntOfArray(myArr, 4);
 	
 	}
-
-	public static void increaseElrmrntOfArray( int[]inArr, int inElenent){
+	public static void increaseElementOfArray( int[]inArr, int inElenent){
 		
-		
+		//дан массив:
+		//6,44,5,14,0,11
+		//нам задают отыскать элемент номер 3 и увеличить его значение на 10%
+	    // обращаемся к переменной хранящей элемент и увеличиваем значение на 10%
+	
 	}
-
-	
-	
 }
