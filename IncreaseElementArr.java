c3a1c51f0f86b964a0b32707f2c9ab0c4497cdb4
megaixin <megaixin12@mail.ru>
2017-05-26 10:40:01 +0300

    start

---
+++ IncreaseElementArr.java
@@ -0,0 +1,17 @@
+//Напишите метод, который будет увеличивать заданный элемент массива на 10%.
+class IncreaseElementArr{
+	public static void maim (String[]args){
+
+		int [] myArr = new int {6,44,5,4,14,0,11};
+	
+	
+	}
+
+	public static void increaseElrmrntOfArray( int[]inArr, int inElenent){
+		
+		
+	}
+
+	
+	
+}
--- NullArray.java
+++ NullArray.java
@@ -2,6 +2,9 @@
 class NullArray{
 	public static void maon (String[]args){
 		
+		int [] myArr = new int {6,0,5,5,4,10,0,1};
+		
+		
 		
 	}
 }
