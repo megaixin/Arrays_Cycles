97ad028bdbdd3076f79999ba98521e602a5dbd4c
megaixin <megaixin12@mail.ru>
2017-05-31 14:56:30 +0300

    new

---
+++ RandomNumbersOfArr.java
@@ -0,0 +1,13 @@
+//Заполнить массив из 10 элементов случайными числами.
+class RandomNumbersOfArr{
+	public static void main (String [args]){
+		int [] myArray = new int [10];
+		
+		
+		
+		
+		
+		
+	}
+	
+}
