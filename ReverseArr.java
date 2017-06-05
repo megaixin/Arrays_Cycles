8430e505faa526eb9927405fa73b1d2b759f846b
megaixin <megaixin12@mail.ru>
2017-06-05 17:50:06 +0300

    full

--- ReverseArr.java
+++ ReverseArr.java
@@ -1,58 +1,27 @@
-//Проверить, отсортирован ли массив в обратном порядке.
+п»ї//РџСЂРѕРІРµСЂРёС‚СЊ, РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅ Р»Рё РјР°СЃСЃРёРІ РІ РѕР±СЂР°С‚РЅРѕРј РїРѕСЂСЏРґРєРµ.
 class ReverseArr{
 	public static void main(String []args){
 		
-		//два разных массива для проверки
-		int []myReverseSortArr = {2,1,3,4,5,6};
-		//int [] myReverseSortArr = {6,5,4,3,2,1};
-
-		boolean isSort = true;
-
-		for (int i = 0; i<myReverseSortArr.length-1;i++){
-			//если каждое последующее число массива меньше предыдущего  -
-			if (myReverseSortArr[i+1]<=myReverseSortArr[i]){
-				//массив считаем отсортированным
-				isSort = true;
-			}
-			//если в массиве хоть раз окажется последующее число больше предыдущего -
-			if (myReverseSortArr[i+1]>myReverseSortArr[i]){
-				//массив считаем неотсортированным.
-				isSort = false;
-				//выход из цикла
-				break;
-			}
-			
-		}		
-		System.out.print(isSort);
+		//РґРІР° СЂР°Р·РЅС‹С… РјР°СЃСЃРёРІР° РґР»СЏ РїСЂРѕРІРµСЂРєРё
+		//int []myReverseSortArr = {2,1,3,4,5,6};
+		int [] myReverseSortArr = {6,5,4,3,2,1};
+		verifyReverseSortOfArray(myReverseSortArr);
+	
 	}	
 
 	public static void verifyReverseSortOfArray(int [] inArray){
-		
 		boolean isSort = true;
 		
 		for (int i = 0; i<inArray.length-1;i++){
 			if (inArray[i+1]<=inArray[i]){
-				inSort = true;
+				isSort = true;
+			}
+		
+			if (inArray[i+1]>inArray[i]){
+				isSort = false;
+				break;
 			}
 		}
-		if (inArray[i+1]>inArray[i]){
-		isSort = false;
-		
-		
-		
-		}
-		
-		
-	}
-	
-	
-
-	
-	
-
-	
-	
-	
-	
-	
-} 
+		System.out.print(isSort);
+	}	
+}
