db4bce40aebff1068ef82fa6d02711b5b59fd110
megaixin <megaixin12@mail.ru>
2017-06-13 10:57:56 +0300

    example work start

---
+++ E001FirstSortedCode.java
@@ -0,0 +1,15 @@
+class E001FirstSortedCode{
+	
+	
+	
+	
+	public static void main(String[] inArgs){
+		
+		
+		System.out.println("Привет, мир");
+	}
+	
+	
+	
+	
+}
---
+++ E001_FirstSortedCode.java
@@ -0,0 +1,15 @@
+﻿class E001_FirstSortedCode{
+	
+	
+	
+	
+	public static void main(String[] inArgs){
+		
+		
+		System.out.println("Привет, мир");
+	}
+	
+	
+	
+	
+}
