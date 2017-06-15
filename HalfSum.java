a0c650a4dda3991498c537ef2396a191e8e9b7b4
megaixin <megaixin12@mail.ru>
2017-06-15 15:39:24 +0300

    try2

--- HalfSum.java
+++ HalfSum.java
@@ -3,22 +3,24 @@ class HalfSum{
 	public static void main (String []args){
 		
 		int [] myArr = {9,3,12,6,10,3};
+		
 		//пробуем создать массив типа double с такой же длинной как у массива типа int 
-		double[] myCloneArr = new double[myArr.length];
-		//клонируем чтобы в него параметры массива типа int
-		System.arraycopy(myArr, 0, myCloneArr, 0, myArr.length);
-		//пытаемся вывести double типа массив, терпим крах из-за проблем с преобразованием
-	
+		double[] myDoubleArr = new double[myArr.length];
+		
+		//пробуем преобразовать целочисленный тип массива в дробный тип
+		for (int i = 0; i < myArr.length; i++){
+			myDoubleArr[i] = (double) myArr[i];
+		}
 		
 		
+		//заменяем все элементы кроме крайних на полусумму соседей текущего числа целочисленного массива
 		for (int i = 1;i<myArr.length-1;i++){  
 			myArr[i] = (myArr[i-1] + myArr[i+1])/2;
-		
 		}
 
-		
-		writeArrayOnConsole(myCloneArr);
 	}
+	
+	
 	public static void writeArrayOnConsole(int[] innerMassive){
 		//System.out.println("Выводим массив: ");
 		for (int i = 0; i<innerMassive.length;i++){
