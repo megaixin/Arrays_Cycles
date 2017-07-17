3cfbd147a9f58020c9f4e0638947133630734b81
megaixin <megaixin12@mail.ru>
2017-07-17 16:44:12 +0300

    done

--- LineArray.java
+++ LineArray.java
@@ -3,29 +3,27 @@
 class LineArray{
 	public static void main (String []args){
 		
-		int []myArray = {1,5,7,12,3,90,7,16,82,0};//10 символов
-		
-		int myNewElement = 9;
+		int []myArray = {1,5,7,12,3,90,7,16,82};//10 символов
+		//переменная гле мы задаем число
+		int myNewElement = 11;
+		//выводим изначальный массив на консоль
 		writeArrayOnConsole(myArray);
-
+		//сдвигаем массив и добавляем число в конец помощью метода
 		addElemetnToEndOfArray(myArray, myNewElement);
+		//выводим измененный массив на консоль
 		writeArrayOnConsole(myArray);
-		
-		/*
-		{5,7,12,3, } - ЗНАЧЕНИЕ ИЗ 1-Й ЯЧЕЙКИ ЛОЖИТСЯ В НУЛЕВУЮ, ЗНАЧЕНИЕ ИЗ 2-Й - В 1-Ю и т.д.
-		алгоритм 
-		цикл
-		переменная под новое число в атрибутах
-		клонирование значений, то есть здвигание
-		*/
 		
 	}
 	public static void addElemetnToEndOfArray (int []inArr, int inNewElement){
-		for(int i =0; i<inArr.length-1;i++){
-			inArr[i]=inArr[i+1];
-			inArr[inArr.length-1] = inNewElement;
+		for(int i = 0; i<inArr.length-1;i++){ //исключаем переполнение
+			//значение текущего элемента равно значению последующего элемента
+			inArr[i] = inArr[i+1];
 		}
+		//последнее значение элемента равно значению заданному в переменной
+		inArr[inArr.length-1] = inNewElement;
 	}
+	
+	
 	public static void writeArrayOnConsole(int[] innerMassive){
 		//System.out.println("Выводим массив: ");
 		for (int i = 0; i<innerMassive.length;i++){
