bd830e82327be0c1c6f1d9efae6e6c5ebecae74a
megaixin <megaixin12@mail.ru>
2017-07-19 17:04:06 +0300

    2 part done

--- myArrayCopy.java
+++ myArrayCopy.java
@@ -5,25 +5,25 @@ class myArrayCopy{
 		int[] mySourceArray = {1,1,1,1,1};
 		int[] myDestinationArray = {2,2,2,2,2};
 
-		System.out.println("n/Вывоводим массив-оригинал mySourceArray: ");
+		System.out.println("Вывоводим массив-оригинал mySourceArray: ");
 		writeArrayOnConsole(mySourceArray);
-		System.out.println("n/Вывоводим массив для копирования myDestinationArray ДО изменений : ");
+		
+		System.out.println("Вывоводим массив для копирования myDestinationArray ДО изменений : ");
 		writeArrayOnConsole(myDestinationArray);
 		
-		
-		for(int i =0; i<mySourceArray.length;i++){
-			myDestinationArray[i] = mySourceArray[i];
-		}
+		//используем метод копирования массива
+		myCopyArrayMethod(mySourceArray, myDestinationArray);
+
 		System.out.println("Вывоводим массив myDestinationArray после копирования: ");
 		writeArrayOnConsole(myDestinationArray);
-		
-	/*
-	цикл будет делать так:
-	начиная с нуля  пока не кончится длинна массива оригинала итератор плюс
-*/
+
 	}
  
- 
+	public static void myCopyArrayMethod(int []inSourceArray, int []inDestinationArray){
+		for(int i = 0; i<inSourceArray.length;i++){
+			inDestinationArray[i] = inSourceArray[i];
+		}
+	}
 
 	public static void writeArrayOnConsole(int[] innerMassive){
 		//System.out.println("¬ыводим массив: ");
