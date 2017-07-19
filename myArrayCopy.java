5bba7da118216cfc5be19d7647425a01d6d851b2
megaixin <megaixin12@mail.ru>
2017-07-19 17:46:21 +0300

    try done

--- myArrayCopy.java
+++ myArrayCopy.java
@@ -8,29 +8,30 @@ class myArrayCopy{
 		System.out.println("Вывоводим массив-оригинал mySourceArray: ");
 		writeArrayOnConsole(mySourceArray);
 		
-		System.out.println("Вывоводим массив для копирования myDestinationArray ДО изменений : ");
+		System.out.println("Вывоводим массив для копирования myDestinationArray ДО изменений: ");
 		writeArrayOnConsole(myDestinationArray);
 		
 		//используем метод копирования массива
-		myCopyArrayMethod(mySourceArray, 2, myDestinationArray, 2);
+		myCopyArrayMethod(mySourceArray, 2, myDestinationArray, 2, 5);
 
 		System.out.println("Вывоводим массив myDestinationArray после копирования: ");
 		writeArrayOnConsole(myDestinationArray);
 
 	}
  
-	public static void myCopyArrayMethod(int []inSourceArray, int innerSourseElement, int []inDestinationArray, int innerDestinationElement){
-		for(int i = 0; i<inSourceArray.length;i++){
-			inDestinationArray[innerDestinationElement] = inSourceArray[innerSourseElement];
+	public static void myCopyArrayMethod(int []inSourceArray, int innerSourseElement, int []inDestinationArray, int innerDestinationElement, int length){
+		
+		for(int i = innerSourseElement; i<inSourceArray.length;i++){
+			inDestinationArray[i] = inSourceArray[i];
+		
 		}
 	}
 
 	public static void writeArrayOnConsole(int[] innerMassive){
-		//System.out.println("¬ыводим массив: ");
+		//System.out.println("Выводим массив: ");
 		for (int i = 0; i<innerMassive.length;i++){
 			System.out.print(innerMassive[i]+" ");
 		}
 		System.out.println();
 	}
-
  }
