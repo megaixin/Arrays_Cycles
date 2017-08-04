b896274ea546a772958fbd4d4bb96dd53b16b4dd
megaixin <megaixin12@mail.ru>
2017-08-04 11:54:30 +0300

    comments

--- Drawing.java
+++ Drawing.java
@@ -4,32 +4,31 @@ import java.awt.Color;
 class Drawing{
 	
 	public static void main (String[]args){
-		
+		//делаем фрейм
 		JFrame frame = new JFrame("Drawing frame");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(50, 10, 305, 450);
 		frame.getContentPane().setBackground(Color.GRAY);
 		frame.setLayout(null);
 		
-		
+		//делаем панель
 		JPanel panel = new JPanel();
 		panel.setBounds(10, 7, 270, 350);
 		panel.setBackground(Color.WHITE);
 		
+		//делаем две кнопки: одна будет рисовать другая очищать панель от нарисованного
 		JButton cleaningButton = new JButton();
 		cleaningButton.setBounds(10,360,130,50);
-		
 		
 		JButton drawingButton = new JButton();
 		drawingButton.setBounds(150,360,130,50);
 		
+		//добавляем панель и кнопки на фрейм
 		frame.add(panel);
 		frame.add(cleaningButton);
 		frame.add(drawingButton);
 		
-		
-		
-		
+		//делаем видимыми все обьекты
 		frame.setVisible(true);
 		panel.setVisible(true);
 		cleaningButton.setVisible(true);
