71e7ad53903047cbf25327909ab6f33b92a7d280
megaixin <megaixin12@mail.ru>
2017-08-07 11:14:27 +0300

    make method

--- MyCustomPaintPanel.java
+++ MyCustomPaintPanel.java
@@ -5,13 +5,13 @@ import java.awt.Graphics;
 class MyCustomPaintPanel extends JPanel{
 
 
-	@Override
-	public void paint(Graphics g){
-		super.paint(g);
-		
-		//вписываешь свой код
-		//например 
-   
+ @Override
+ public void paint(Graphics g){
+	super.paint(g);
+	myPaintMethod(g); 
+ }
+
+	public void myPaintMethod(Graphics g){
 		g.setColor(Color.BLACK);
 		g.drawLine(130, 100, 200, 5);  
 		g.drawLine(60, 200, 15, 15);
@@ -20,8 +20,5 @@ class MyCustomPaintPanel extends JPanel{
 		g.drawOval(50, 50, 40, 50);
 		g.setColor(Color.RED);
 		g.drawOval(70, 60, 30, 40);
-	
 	}
-
-	
 }
