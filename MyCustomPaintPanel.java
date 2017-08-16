9a240cf697019f552b86959c1bceb710e7883a1a
megaixin <megaixin12@mail.ru>
2017-08-16 12:32:18 +0300

    svastik

--- MyCustomPaintPanel.java
+++ MyCustomPaintPanel.java
@@ -22,15 +22,29 @@ class MyCustomPaintPanel extends JPanel{
 		Graphics imgCanvas = myBuffImage.getGraphics();
 		imgCanvas.drawLine(5, 5, 17, 12);
 		
-		
-		for(int i = 1; i<19;i++){
-			myBuffImage.setRGB(9, 1*i,0xFF0000);
-			
-		}
-		
+		//горизонтальная основная
 		for(int i = 1; i<19;i++){
 			myBuffImage.setRGB(  i , 9 ,0xFF0000);
 		}
+		for(int i = 10; i<12;i++){
+			//правая смотрит вниз вертикальная
+			myBuffImage.setRGB(18, 1*i, 0xFF0000);
+			//левая смотрит вверх вертикальная
+			myBuffImage.setRGB(20 - 19, 18 - 1*i, 0xFF0000); 
+		}
+		//вертикальная основная
+		for(int i = 1; i<19;i++){
+			myBuffImage.setRGB(  9 , 1*i ,0xFF0000);
+		}
+		
+		for(int i = 9; i<12;i++){
+			//верхняя смотрит впрево горизонтальная
+			myBuffImage.setRGB(i, 1, 0xFF0000);
+			
+			//нижняя смотрит вправо горизонтальная
+			myBuffImage.setRGB(i-2, 18, 0xFF0000); 
+		}
+		
 		
 		g.drawImage(myBuffImage, 10, 15, 200+10, 200+15,
 		0, 0, 20, 20,
